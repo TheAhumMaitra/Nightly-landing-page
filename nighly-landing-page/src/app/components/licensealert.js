91ff651d60b8f0336af145e99756b3183fa46bb0
Ahum Maitra <theahummaitra@gmail.com>
2025-09-07 12:43:17 +0530

    Fixed licensealert - ' error

--- nighly-landing-page/src/app/components/licensealert.js
+++ nighly-landing-page/src/app/components/licensealert.js
@@ -3,7 +3,7 @@ export default function LicenseAlert() {
     <div role="alert" className="alert alert-info alert-outline !p-2">
       <div className="badge badge-primary !p-4 font-bold">License</div>
       <span>
-        MIT*. '*' - View License file (GitHub) for more information!
+        MIT*. Note : ..*.. - View License file (GitHub) for more information!
       </span>
     </div>
   );
